e0ec2fee6690ce3b006f56b90231920d131fa222
Chantel Williams <cewilliams095@gmail.com>
2024-03-17 13:41:07 -0400

    CreateGroundWaterFlowScript

---
+++ web-projects/GroundWaterTransport/GroundWaterTransport.py
@@ -0,0 +1,8 @@
+#Name: GroundWaterTransport
+#Author : Chantel Williams
+#Start Date : 3/17/2024
+#Purpose: create flexible class that creates groundwater flow and transport site based on defined spatial inputs
+
+import flopy
+import numpy
+
